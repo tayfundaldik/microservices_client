fd252ebdf6a7e37f0fe796ae185c242d289ca73d
Tayfun Daldık <79011413+tayfundaldik@users.noreply.github.com>
2023-05-23 17:40:36 +0300

    Adding Dtos

---
+++ src/main/java/com/datwashurtt/miscroservicesclient/microservicesclient/web/model/CustomerDto.java
@@ -0,0 +1,8 @@
+package com.datwashurtt.miscroservicesclient.microservicesclient.web.model;
+
+import java.util.UUID;
+
+public class CustomerDto {
+    private UUID id;
+    private String customerName;
+}
---
+++ src/test/java/com/datwashurtt/miscroservicesclient/microservicesclient/MicroservicesClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datwashurtt.miscroservicesclient.microservicesclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
